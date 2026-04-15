6089d2f899f438998324edf3b72a2df91ae6484b
Julian Schmid <218584912+julian4schmid@users.noreply.github.com>
2026-04-15 14:57:26 +0200

    continue to build the reports, also make some improvements

--- scripts/generate_reports.py
+++ scripts/generate_reports.py
@@ -36,62 +36,62 @@ def generate_single_report(result, data):
     # =========================
 
     # sender
-    ws["F1"] = building["sender"]
-    ws["F2"] = building.get("sender_address", "")
+    ws["F1"] = building.get("sender")
+    ws["F2"] = building.get("sender_address") or ""
 
     # bank details
-    ws["F5"] = building["owner"]
-    ws["F6"] = f"IBAN: {building["bank_account"]}"
-    ws["F7"] = building.get("bank", "")
+    ws["F5"] = building.get("owner")
+    ws["F6"] = f"IBAN: {building.get("bank_account")}"
+    ws["F7"] = building.get("bank") or ""
 
     # address
     ws["A7"] = build_recipients_name(tenant, 1)
     ws["A8"] = build_recipients_name(tenant, 2)
-    ws["A9"] = building["address"]
-    ws["A10"] = building["city_line"]
+    ws["A9"] = building.get("address")
+    ws["A10"] = building.get("city_line")
     if tenant.get("new_address"):
-        ws["A9"] = tenant["new_address"]
-        ws["A10"] = tenant["new_city_line"]
+        ws["A9"] = tenant.get("new_address")
+        ws["A10"] = tenant.get("new_city_line")
 
     # general information
     ws["F10"] = "Wohn- und Geschäftshaus" if building.get("has_shops", False) else "Wohnhaus"
-    ws["F11"] = f"{building["address"]}, {building["city_line"]}"
+    ws["F11"] = f"{building.get("address")}, {building.get("city_line")}"
     period = get_billing_period(building, year)
     ws["F12"] = (f"Abrechnungszeitraum: "
                  f"{period[0].strftime("%d.%m.%y")} - {period[1].strftime("%d.%m.%y")}")
 
     # information for calculation
-    ws["H20"] = tenant.get("prepay_ops", 0)
-    ws["H19"] = result["months"]
+    ws["H20"] = tenant.get("prepay_ops") or 0
+    ws["H19"] = result.get("months")
     row = 18
     if not building["is_single_unit"]:
         ws[f"F{row}"] = "Ihre Wohnfläche:" if not is_shop else "Ihre Nutzfläche:"
-        ws[f"H{row}"] = unit["area"]
+        ws[f"H{row}"] = unit.get("area")
         ws[f"I{row}"] = "qm"
         row -= 1
         ws[f"F{row}"] = "Ihre Wohnung:" if not is_shop else "Ihre Lage:"
-        ws[f"H{row}"] = unit["position"]
+        ws[f"H{row}"] = unit.get("position")
         row -= 1
 
         if building.get("gar_count", 0) > 0:
             ws[f"F{row}"] = "Anzahl Garagen:"
-            ws[f"H{row}"] = building["gar_count"]
+            ws[f"H{row}"] = building.get("gar_count")
             row -= 1
 
         if building.get("unit_count", 0) > 0:
             ws[f"F{row}"] = "Anzahl Wohnungen:"
-            ws[f"H{row}"] = building["unit_count"]
+            ws[f"H{row}"] = building.get("unit_count")
             row -= 1
 
         if building.get("total_tenant_area", 0) > 0:
             ws[f"F{row}"] = "Gesamtwohnfl.:"
-            ws[f"H{row}"] = building["total_tenant_area"]
+            ws[f"H{row}"] = building.get("total_tenant_area")
             ws[f"I{row}"] = "qm (*)"
             row -= 1
 
         if building.get("total_area", 0) > 0:
             ws[f"F{row}"] = "Gesamtwohnnutzfl.:" if building.get("has_shops", False) else "Gesamtwohnfl.:"
-            ws[f"H{row}"] = building["total_area"]
+            ws[f"H{row}"] = building.get("total_area")
             ws[f"I{row}"] = "qm"
             row -= 1
 
@@ -99,16 +99,30 @@ def generate_single_report(result, data):
     # COST TABLE
     # =========================
 
-    start_row = 20
-    current_row = start_row
+    # header
+    header_row = 22
+    if building["is_single_unit"]:
+        ws[f"I{header_row}"] = "Kosten/€"
+    else:
+        ws[f"E{header_row}"] = "Gesamtkosten/€"
+        ws[f"G{header_row}"] = "Verteilt"
+        ws[f"I{header_row}"] = "Ihr Anteil/€"
 
+    # costs
+    row = header_row + 2
     for line in result["lines"]:
-        ws[f"A{current_row}"] = line.get("cost_type", "")
-        ws[f"B{current_row}"] = line.get("allocation", "")
-        ws[f"C{current_row}"] = line.get("share", "")
-        ws[f"D{current_row}"] = line.get("amount", 0)
+        if line["amount"] > 0:
+            ws[f"A{row}"] = line.get("cost_type")
+            ws[f"I{row}"] = line.get("amount")
 
-        current_row += 1
+            if not building["is_single_unit"]:
+                ws[f"E{row}"] = line.get("total_amount") or ""
+                ws[f"G{row}"] = line.get("allocation") or ""
+
+            row += 1
+
+
+
 
     # =========================
     # BORDER BEFORE TOTAL
